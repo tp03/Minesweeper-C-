18e6bfe9686b0490c5fc2be561b75e3a84f4b4a9
akowals2 <01178230@pw.edu.pl>
2023-05-15 12:53:46 +0200

    added test file

---
+++ test_board.cpp
@@ -0,0 +1,21 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "board.h"
+#include <stdexcept>
+#include <cassert>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace testhistogram
+{
+	TEST_CLASS(testhistogram)
+	{
+	public:
+
+		TEST_METHOD()
+        {
+            
+        }
+
+	};
+}
