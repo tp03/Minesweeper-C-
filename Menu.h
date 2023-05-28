0d98d859e2c0d09ff899490c1b1eb80ddb6410c0
iploszaj <01178242@pw.edu.pl>
2023-05-28 14:59:21 +0200

    Created Menu header file

--- Menu.h
+++ Menu.h
@@ -1,6 +1,27 @@
 #pragma once
+#include "SFML/Graphics.hpp"
+#include <iostream>
+
+#define BUTTONS_NUMBER 3
 
 class Menu
 {
+	int width, height;
+	int selectedButtonIndex;
 
+	sf::Font font;
+	sf::Text buttons[BUTTONS_NUMBER];
+	sf::RenderWindow* window;
+
+	void drawMenu();
+	void upKeyAction();
+	void downKeyAction();
+	void enterKeyAction();
+	void playButtonAction();
+	void rankingButtonAction();
+
+public:
+	Menu(int width, int height, sf::RenderWindow* window);
+
+	int getSelectedButtonIndex();
 };
